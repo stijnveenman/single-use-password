e68a6c94ea85f347e9f0aba6500305157d8e63b3
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-28 12:35:47 +0100

    added sqlx basic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use serde_json::{json, Value};
+use sqlx::{postgres::PgPoolOptions, types::Uuid};
 use thiserror::Error;
 
 #[tokio::main]
@@ -7,6 +8,21 @@ async fn main() {
     let app = Router::new()
         .route("/json", get(json))
         .route("/failing", get(failing));
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost:5433/single-use-password")
+        .await
+        .unwrap();
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let row: Vec<(Uuid, String, String)> = sqlx::query_as("SELECT * FROM passwords")
+        .bind(150_i64)
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    println!("{:?}", row);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
