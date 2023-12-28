6bbbafbc2b63e64b74d8fe22c6b5a6f7730dcf2c
Stijn Veenman <stijn.veenman@schiphol.nl>
2023-12-28 12:17:00 +0100

    made a cusom error object

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use axum::{routing::get, Json, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use serde_json::{json, Value};
+use thiserror::Error;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/json", get(json));
+    let app = Router::new()
+        .route("/json", get(json))
+        .route("/failing", get(failing));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -14,4 +17,26 @@ async fn main() {
 // that implements `serde::Serialize`
 async fn json() -> Json<Value> {
     Json(json!({ "data": 42 }))
+}
+
+#[derive(Error, Debug)]
+enum AppError {
+    #[error("Random intermittent error")]
+    Random,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            AppError::Random => (
+                StatusCode::BAD_REQUEST,
+                Json(json!({"error":"Random error"})),
+            ),
+        }
+        .into_response()
+    }
+}
+
+async fn failing() -> Result<Json<Value>, AppError> {
+    Err(AppError::Random)
 }
