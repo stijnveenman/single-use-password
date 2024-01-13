4ea46d0d852444eb3062e3333b62dd71cf0d218f
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-13 16:18:03 +0100

    added create password endpoint

--- src/app_result.rs
+++ src/app_result.rs
@@ -6,17 +6,20 @@ use thiserror::Error;
 pub enum AppError {
     #[error("Random intermittent error")]
     Random,
+    #[error("This feature is not implemented yet")]
+    NotImplemented,
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
-        match self {
-            AppError::Random => (
-                StatusCode::BAD_REQUEST,
-                Json(json!({"error":"Random error"})),
-            ),
-        }
-        .into_response()
+        (
+            match self {
+                AppError::Random => StatusCode::BAD_REQUEST,
+                AppError::NotImplemented => StatusCode::BAD_REQUEST,
+            },
+            Json(json!({"error":self.to_string()})),
+        )
+            .into_response()
     }
 }
 
--- src/passwords/router.rs
+++ src/passwords/router.rs
@@ -1,11 +1,11 @@
 use axum::{extract::State, routing::get, Json, Router};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::{app_context::AppContext, app_result::AppResult};
 
 pub fn router() -> Router<AppContext> {
-    Router::new().route("/", get(get_passwords))
+    Router::new().route("/", get(get_passwords).post(create_password))
 }
 
 #[derive(Serialize)]
@@ -22,4 +22,16 @@ async fn get_passwords(State(context): State<AppContext>) -> AppResult<Vec<Passw
         .unwrap();
 
     Ok(Json(data))
+}
+
+#[derive(Deserialize)]
+struct CreatePassword {
+    password: String,
+}
+
+async fn create_password(
+    State(context): State<AppContext>,
+    Json(payload): Json<CreatePassword>,
+) -> AppResult<Password> {
+    Err(crate::app_result::AppError::NotImplemented)
 }
