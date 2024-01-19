a2e6a9c409569c47b5509d37175717f1b88ff37f
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-19 09:01:30 +0100

    fixed double password struct

--- src/password.rs
+++ src/password.rs
@@ -1,16 +1,8 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[cfg(feature = "ssr")]
-#[derive(Serialize, Deserialize, sqlx::FromRow, Debug, PartialEq, Eq, Clone)]
-pub struct Password {
-    pub id: Uuid,
-    pub key: String,
-    pub password: String,
-}
-
-#[cfg(not(feature = "ssr"))]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
 pub struct Password {
     pub id: Uuid,
     pub key: String,
